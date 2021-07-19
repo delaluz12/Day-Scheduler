a4a9d8a8860d211f23d42fdc6ecebea13d13383b
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-07-19 14:42:19 -0500

    added timeblocks and displayDate function plus minor pseudo code

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
 // DOM variables 
 
+//global variables
+var currentDate;
+var currentTime;
 
+//function that displays current day 
+function displayDate (){
+    currentDate = moment().format('dddd, MMMM Do')
+    console.log(currentDate);
+    $('#currentDay').html(currentDate);
+}
+
+//display information on timeblocks for schedule
+function renderTasks () {
+    
+}
+
+//
+
+displayDate();
